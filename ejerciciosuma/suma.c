48c0ab6938879f312dfa0cdaa3ef5920fa090e41
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-18 14:18:06 -0500

    Añadir función de suma por referencia usando punteros -Mateo Mero

--- ejerciciosuma/suma.c
+++ ejerciciosuma/suma.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-
 int suma(int a, int b);
+int sumaxref(int *ptra, int *ptrb);
 
-int main(int argc, char *argv[]) {
-    
-    int a, b;
-    
+int main (int argc, char *argv[]) {
+    int a,b;
     printf("Ingrese el primer numero: ");
-    scanf("%d", &a);
-    
+    scanf("%d",&a);
     printf("Ingrese el segundo numero: ");
-    scanf("%d", &b);
-    
-    printf("El resultado es: %d", suma(a,b));
-    
+    scanf("%d",&b);
+    printf("El resultado es: %d\n", suma(a,b));
+    printf("El resultado es: %d", sumaxref(&a,&b));
     return 0;
 }
 
-int suma(int a, int b) {
+int suma(int a, int b){
     return a+b;
+}
+
+int sumaxref(int *ptra, int *ptrb){
+    return *ptra + *ptrb;
 }
